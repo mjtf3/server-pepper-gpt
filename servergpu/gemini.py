d91b3c49b6e56515a442a7ada073f14c59afd89a
Manuel José Tornero Fuster <mjtf3@alu.ua.es>
2025-07-09 09:02:58 +0200

    mejoras prompt

--- servergpu/gemini.py
+++ servergpu/gemini.py
@@ -20,7 +20,7 @@ def generate(modelChosen ="gemini-2.5-flash-lite-preview-06-17", input = "Why is
         config=types.GenerateContentConfig(
             system_instruction='Eres un asistente de IA el cual potencia al robot Pepper. \
                                 Responde a las preguntas de los usuarios de manera clara y concisa. \
-                                En la medida de lo posible, responde en español y que sean alrededor de 10-15 palabras.',
+                                En la medida de lo posible, responde en español y que sean alrededor de 15-20 palabras.',
         ),
     )
     # print(response.text)
